167accc549b38a3d63121437426cb16377b7ff53
gustavorods <gustavo.rlsilva07@gmail.com>
2024-04-21 21:19:42 -0300

    feat: image change with desctiption

--- srcipt.js
+++ srcipt.js
@@ -28,12 +28,16 @@ function showData (data) {
     let cityMaxTemp = document.querySelector("#maxTemp");
     let moisture = document.querySelector("#moisture");
     let wind = document.querySelector("#wind");
+    let description = document.querySelector("#description");
+    let imageWeatherConditions = document.querySelector("#weatherConditions");
     cityName.innerHTML = data.name; 
     cityTemp.innerHTML = `${parseInt(data.main.temp)} <sup>°C </sup>`;
     cityMinTemp.innerHTML = `${parseInt(data.main.temp_min)} <sup>°C </sup>`;
     cityMaxTemp.innerHTML = `${parseInt(data.main.temp_max)} <sup>°C </sup>`;
     moisture.innerHTML = `${parseInt(data.main.humidity)}%`;
     wind.innerHTML = `${parseInt(data.wind.speed)}km/h`;
+    description.innerHTML = `${data.weather[0].description}`;
+    imageWeatherConditions.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
 }   
 
 
